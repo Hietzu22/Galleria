0bb5b9c4fe56933e4e17145fbf5765ac67491594
Hietzu22 <veeti.hietanen03@gmail.com>
2021-11-19 09:04:57 +0200

    updated homepage link

--- Galleria.php
+++ Galleria.php
@@ -17,7 +17,7 @@
     <div class="navbar">
         <a href="Lataa.php">Lataa</a>
         <a href="Galleria.php">Galleria</a>
-        <a href="http://it.esedu.fi/~hietanen.veeti/Hietzu22_Kotisivu/index.html">Takaisin kotisivulle</a>
+        <a href="http://it.esedu.fi/~hietanen.veeti/index.php">Takaisin kotisivulle</a>
         <a href="login.php">Admin view</a>
         <br>
     </div>
